8d826c014964594424be87d9e6493304ca04da7c
kanoemon <kanoemon.cho@gmail.com>
2020-10-15 23:13:38 +0900

    明白な実装

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -5,8 +5,11 @@ describe 'Money' do
   describe 'multiplication' do
     it '$5 * 2 = $10になること' do
       five = Dollar.new(5)
-      five.times(2)
-      expect(10).to eq five.amount
+      product = five.times(2)
+      expect(10).to eq product.amount
+
+      product = five.times(3)
+      expect(15).to eq product.amount
     end
   end
 end
