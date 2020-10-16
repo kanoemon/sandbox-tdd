435acbed0b2e26d21610f15f5fd80ef10065bc13
kanoemon <kanoemon.cho@gmail.com>
2020-10-16 14:46:03 +0900

    Factory Methodパターンを導入

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -1,34 +1,35 @@
 require 'spec_helper'
+require_relative '../src/money'
 require_relative '../src/dollar'
 require_relative '../src/franc'
 
 describe 'Money' do
   describe 'multiplication' do
     it '掛け算が正常に行われること' do
-      five = Dollar.new 5
-      expect(Dollar.new 10).to be_equals(five.times 2)
-      expect(Dollar.new 15).to be_equals(five.times 3)
+      five = Money.dollar 5
+      expect(Money.dollar 10).to be_equals(five.times 2)
+      expect(Money.dollar 15).to be_equals(five.times 3)
     end
   end
 
   describe 'equality' do
     it 'オブジェクトが等しいこと' do
-      expect(Dollar.new 5).to be_equals(Dollar.new 5)
-      expect(Franc.new 5).to be_equals(Franc.new 5)
+      expect(Money.dollar 5).to be_equals(Money.dollar 5)
+      expect(Money.franc 5).to be_equals(Money.franc 5)
     end
 
     it 'オブジェクトが等しくないこと' do
-      expect(Dollar.new 5).not_to be_equals(Dollar.new 6)
-      expect(Franc.new 5).not_to be_equals(Franc.new 6)
-      expect(Dollar.new 5).not_to be_equals(Franc.new 5)
+      expect(Money.dollar 5).not_to be_equals(Money.dollar 6)
+      expect(Money.franc 5).not_to be_equals(Money.franc 6)
+      expect(Money.dollar 5).not_to be_equals(Money.franc 5)
     end
   end
 
   describe 'Franc multiplication' do
     it '掛け算が正常に行われること' do
-      five = Franc.new 5
-      expect(Franc.new 10).to be_equals(five.times 2)
-      expect(Franc.new 15).to be_equals(five.times 3)
+      five = Money.franc 5
+      expect(Money.franc 10).to be_equals(five.times 2)
+      expect(Money.franc 15).to be_equals(five.times 3)
     end
   end
 end
--- src/money.rb
+++ src/money.rb
@@ -4,4 +4,12 @@ class Money
   def equals? object
     @amount === object.amount && self.class.name === object.class.name
   end
+
+  def self.dollar amount
+    Dollar.new amount
+  end
+
+  def self.franc amount
+    Franc.new amount
+  end
 end
